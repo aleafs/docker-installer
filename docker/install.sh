0fd8b8796632f7585867e0ea88da90a0ae3716da
aleafs <zhangxc83@gmail.com>
2025-02-16 18:08:05 +0800

    update install

--- docker/install.sh
+++ docker/install.sh
@@ -5,9 +5,23 @@ export LANG=en_US.UTF-8
 APPHOME=$(cd -- "$(dirname -- ${0})" && pwd)
 declare -r APPHOME
 
-OFFLINE_PATH="${APPHOME}/binary/$(uname -m)"
-declare -r OFFLINE_PATH
+PKGHOME="${APPHOME}/binary/$(uname -m)"
+declare -r PKGHOME
 
+function offline_install_docker() {
+  if [ -f "${PKGHOME}/docker.tgz" ]; then
+    tar -xzvf "${PKGHOME}/docker.tgz" && \
+    chmod +x "${PKGHOME}/docker/*" && \
+    mv -fv "${PKGHOME}/docker/*" /usr/bin/
+  fi
+}
 
+function offline_install_compose() {
+    if [ -f "${PKGHOME}/docker-compose" ]; then
+      mkdir -p /usr/local/bin/docker-compose && \
+      cp -fv "${PKGHOME}/docker-compose" /usr/local/bin/docker-compose && \
+      chmod +x /usr/local/bin/docker-compose
+    fi
+}
 
-echo ${OFFLINE_PATH}
+echo ${PKGHOME}
