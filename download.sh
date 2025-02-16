50da5f56001665526d4ac2dbdd0f9df97a683167
aleafs <zhangxc83@gmail.com>
2025-02-16 17:59:21 +0800

    开发更新

--- download.sh
+++ download.sh
@@ -10,7 +10,11 @@ PKGHOME="${APPHOME}/docker/binary/"
 DOCKER_URL="https://download.docker.com/linux/static/stable"
 declare -r DOCKER_URL
 
-VERSION="27.5.1"
+COMPOSE_URL="https://github.com/docker/compose/releases/download"
+declare -r COMPOSE_URL
+
+DOCKER_VERSION="27.5.1"
+COMPOSE_VERSION="v2.33.0"
 
 # TODO: checksum
 for arch in "x86_64" "aarch64"; do
@@ -18,8 +22,8 @@ for arch in "x86_64" "aarch64"; do
   mkdir -p "${PKGHOME}/${arch}" && cd "${PKGHOME}/${arch}" || exit 1
 
   curl --output "docker.tgz" \
-    "${DOCKER_URL}/${arch}/docker-${VERSION}.tgz" || exit 2
+    "${DOCKER_URL}/${arch}/docker-${DOCKER_VERSION}.tgz" || exit 2
 
   curl --output "docker-compose" \
-    "https://github.com/docker/compose/releases/download/v2.33.0/docker-compose-linux-${arch}"
+    "${COMPOSE_URL}/${COMPOSE_VERSION}/docker-compose-linux-${arch}" || exit 3
 done
