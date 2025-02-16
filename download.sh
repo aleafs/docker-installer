3b57259dd98607a6d0adbb929d9428a887206f23
aleafs <zhangxc83@gmail.com>
2025-02-16 17:27:24 +0800

    开发更新

--- download.sh
+++ download.sh
@@ -12,8 +12,14 @@ declare -r DOCKER_URL
 
 VERSION="27.5.1"
 
+# TODO: checksum
 for arch in "x86_64" "aarch64"; do
   rm -rf "${PKGHOME:?}/${arch}"
-  mkdir -p "${PKGHOME}/${arch}" && \
-  curl --silent --output "${PKGHOME}/${arch}/docker.tgz" "${DOCKER_URL}/${arch}/docker-${VERSION}.tgz"
+  mkdir -p "${PKGHOME}/${arch}" && cd "${PKGHOME}/${arch}" || exit 1
+
+  curl --output "docker.tgz" \
+    "${DOCKER_URL}/${arch}/docker-${VERSION}.tgz" || exit 2
+
+  curl --output "docker-compose" \
+    "https://github.com/docker/compose/releases/download/v2.33.0/docker-compose-linux-${arch}"
 done
