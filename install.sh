b7fa7e36c11b4e676b08758528754561e6ec7040
aleafs <zhangxc83@gmail.com>
2025-02-19 17:28:40 +0800

    install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -o nounset
+
 export LANG=en_US.UTF-8
 
 APPHOME=$(cd -- "$(dirname -- ${0})" && pwd)
@@ -18,82 +20,51 @@ function offline_install_docker() {
 
 function offline_install_compose() {
     if [ -f "${PKGHOME}/docker-compose" ]; then
+      chmod +x "${PKGHOME}/docker-compose" && \
       mkdir -p /usr/libexec/docker/cli-plugins && \
-      cp -fv "${PKGHOME}/docker-compose" /usr/libexec/docker/cli-plugins/ && \
-      chmod +x /usr/libexec/docker/cli-plugins/docker-compose
+      mv -fv "${PKGHOME}/docker-compose" /usr/libexec/docker/cli-plugins/
     fi
 }
 
-function install() {
-  cat > /usr/lib/systemd/system/docker.socket << EOF
-[Unit]
-Description=Docker Socket for the API
-
-[Socket]
-# If /var/run is not implemented as a symlink to /run, you may need to
-# specify ListenStream=/var/run/docker.sock instead.
-ListenStream=/run/docker.sock
-SocketMode=0660
-SocketUser=root
-SocketGroup=docker
-
-[Install]
-WantedBy=sockets.target
-EOF
-
+function config_system_service() {
   cat > /usr/lib/systemd/system/docker.service << EOF
 [Unit]
 Description=Docker Application Container Engine
 Documentation=https://docs.docker.com
-After=network-online.target docker.socket firewalld.service containerd.service time-set.target
-Wants=network-online.target containerd.service
-Requires=docker.socket
-
+After=network-online.target firewalld.service
+Wants=network-online.target
 [Service]
 Type=notify
 # the default is not to use systemd for cgroups because the delegate issues still
 # exists and systemd currently does not support the cgroup feature set required
 # for containers run by docker
-ExecStart=/usr/bin/dockerd -H fd:// --containerd=/run/containerd/containerd.sock
+ExecStart=/usr/bin/dockerd -H unix://var/run/docker.sock
 ExecReload=/bin/kill -s HUP $MAINPID
-TimeoutStartSec=0
-RestartSec=2
-Restart=always
-
-# Note that StartLimit* options were moved from "Service" to "Unit" in systemd 229.
-# Both the old, and new location are accepted by systemd 229 and up, so using the old location
-# to make them work for either version of systemd.
-StartLimitBurst=3
-
-# Note that StartLimitInterval was renamed to StartLimitIntervalSec in systemd 230.
-# Both the old, and new name are accepted by systemd 230 and up, so using the old name to make
-# this option work for either version of systemd.
-StartLimitInterval=60s
-
 # Having non-zero Limit*s causes performance problems due to accounting overhead
 # in the kernel. We recommend using cgroups to do container-local accounting.
+LimitNOFILE=infinity
 LimitNPROC=infinity
 LimitCORE=infinity
-# Older systemd versions default to a LimitNOFILE of 1024:1024, which is insufficient for many
-# applications including dockerd itself and will be inherited. Raise the hard limit, while
-# preserving the soft limit for select(2).
-LimitNOFILE=1024:524288
-
-# Comment TasksMax if your systemd version does not support it.
-# Only systemd 226 and above support this option.
-TasksMax=infinity
-
+# Uncomment TasksMax if your systemd version supports it.
+# Only systemd 226 and above support this version.
+#TasksMax=infinity
+TimeoutStartSec=0
 # set delegate yes so that systemd does not reset the cgroups of docker containers
 Delegate=yes
-
 # kill only the docker process, not all processes in the cgroup
 KillMode=process
-OOMScoreAdjust=-500
-
+# restart the docker process if it exits prematurely
+Restart=on-failure
+StartLimitBurst=3
+StartLimitInterval=60s
 [Install]
 WantedBy=multi-user.target
 EOF
-  systemctl daemon-reload
-  systemctl --force enable docker.service
-  systemctl restart docker.service
 }
+
+offline_install_docker
+offline_install_compose
+
+systemctl daemon-reload
+systemctl --force enable docker.service
+systemctl restart docker.service
