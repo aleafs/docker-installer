8c587f00eec62391b6cde719edc2ab8f8faa4959
aleafs <zhangxc83@gmail.com>
2025-10-23 12:35:06 +0800

    增加net.ipv4.ip_forward配置

--- script/postinstall.sh
+++ script/postinstall.sh
@@ -2,6 +2,14 @@
 
 export LANG=en_US.UTF-8
 
+if grep -w "net.ipv4.ip_forward" /etc/sysctl.conf > /dev/null; then
+  sed -i.bak -e 's/net.ipv4.ip_forward\s*=\s*0/net.ipv4.ip_forward = 1/' /etc/sysctl.conf
+else
+  echo "net.ipv4.ip_forward = 1" >> /etc/sysctl.conf
+fi
+
+sysctl -p
+
 /bin/systemctl daemon-reload
 /bin/systemctl --force enable docker
 /bin/systemctl restart docker
