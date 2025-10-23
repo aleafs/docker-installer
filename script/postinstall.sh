970d02705af676f22068712ff287258f119033cf
aleafs <zhangxc83@gmail.com>
2025-10-23 13:25:18 +0800

    Add comment

--- script/postinstall.sh
+++ script/postinstall.sh
@@ -5,6 +5,7 @@ export LANG=en_US.UTF-8
 if grep -w "net.ipv4.ip_forward" /etc/sysctl.conf > /dev/null; then
   sed -i.bak -e 's/net.ipv4.ip_forward\s*=\s*0/net.ipv4.ip_forward = 1/' /etc/sysctl.conf
 else
+  echo -e "\n# added by docker" >> /etc/sysctl.conf
   echo "net.ipv4.ip_forward = 1" >> /etc/sysctl.conf
 fi
 
